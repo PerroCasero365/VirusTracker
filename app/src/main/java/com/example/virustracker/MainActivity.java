07df9d0cbb0a7249e2926a6f2b0dae7ee9c8cd81
PerroCasero365 <jorgefutbol1999@hotmail.es>
2020-05-11 17:59:07 +0200

    Añadimos el activity encuesta

--- app/src/main/java/com/example/virustracker/MainActivity.java
+++ app/src/main/java/com/example/virustracker/MainActivity.java
@@ -92,7 +92,10 @@ public class MainActivity extends AppCompatActivity
 
     public void cambiaEstado(View v)
     {
-        redondear(R.drawable.amarillo);
+        Intent intent = new Intent(this, Activity_encuesta.class);
+        startActivity(intent);
+        //redondear(R.drawable.amarillo);
+
     }
 
 
